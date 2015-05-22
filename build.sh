560358632d068a517a47f854af60f43fd39a59db
Chris Joel <cdata@google.com>
2015-05-22 12:52:37 -0700

    Tools.

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -x
+set -e
+
+repo=$(cat ./bower.json | grep -E "name\":" | sed 's/.*name": "\([^"]*\).*/\1/')
+
+rm -rf ./bower_components
+bower install
+bower install polymerelements/iron-ajax#^0.9.0
+
+git subtree add -P $repo origin gh-pages
+
+rm -rf ./$repo/*
+
+cp -r ./bower_components/* ./$repo
+
+cp ./element-page-tools/index.html ./$repo
+cp ./element-page-tools/index.html ./$repo
+cp ./bower.json ./$repo
+
+git add .
+git commit -m "Automated gh-pages deployment."
+
+git subtree push -P $repo origin gh-pages
+git reset --hard origin/master
+
+set +x
+set +e
